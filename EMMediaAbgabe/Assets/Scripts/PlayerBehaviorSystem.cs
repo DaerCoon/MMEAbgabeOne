da68d7a2f88b82b63b3351ddf8c17e0cc976c667
DaerCoon <51750768+DaerCoon@users.noreply.github.com>
2020-12-05 19:19:20 +0100

    Abgabe4 verbessert

--- EMMediaAbgabe/Assets/Scripts/PlayerBehaviorSystem.cs
+++ EMMediaAbgabe/Assets/Scripts/PlayerBehaviorSystem.cs
@@ -7,18 +7,20 @@ using UnityEngine;
 
 public class PlayerBehaviorSystem : SystemBase
 {
+   
     protected override void OnUpdate()
     {
+        var deltaTime = Time.DeltaTime;
         float horizontalM = Input.GetAxis("Horizontal");
         float verticalM = Input.GetAxis("Vertical");
 
         float elapsedTime = (float)Time.ElapsedTime;
         Entities.ForEach((ref PlayerComponent player, ref Translation tran, ref Rotation rot) =>
         {
-            player.RotationAngle += horizontalM;
+            player.RotationAngle += horizontalM * deltaTime;
             float3 direct = new float3(math.sin(player.RotationAngle), 0, math.cos(player.RotationAngle));
             rot.Value = quaternion.LookRotationSafe(direct, Vector3.up);
-            tran.Value += direct * verticalM * player.Speed * elapsedTime;
+            tran.Value += direct * verticalM * player.Speed * elapsedTime* deltaTime;
 
         }).Schedule();
     }
